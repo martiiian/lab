563dd003df4347c3757eee79116dbf36d8592e3d
martiiian <jkramorevx@gmail.com>
2023-03-24 19:44:04 +0300

    Update some

---
+++ javascript/oop/store-pattern/src/pubsub.ts
@@ -0,0 +1,19 @@
+export default class Pubsub {
+  constructor() {
+    this.events = {}
+  }
+
+  subscribe(event, callback) {
+    if (!this.events.hasOwnProperty(event)) {
+      this.events[event] = []
+    }
+    this.events[event].push(callback)
+  }
+
+  publish(event, data = {}) {
+    if (!this.events.hasOwnProperty(event)) {
+      return []
+    }
+    return this.events[event].map((callback) => callback(data))
+  }
+}
